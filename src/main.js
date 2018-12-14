3571f4e2427cbbb2417f2fd9ab14d4cecfdfd6f6
Vangelis Galanis <vaggalanis@gmail.com>
2018-12-14 02:34:01 +0200

    Include font awesome in vue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
