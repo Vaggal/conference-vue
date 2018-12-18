1ea031b9a3fe9747b225ca7d540767bc1452b591
Vangelis Galanis <vaggalanis@gmail.com>
2018-12-18 02:27:43 +0200

    Implement component for user thumbnails, import user icon from font-awesome

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
-import Vue from 'vue'
-import App from '@/App.vue'
-import router from '@/router'
+import Vue from 'vue';
+import App from '@/App.vue';
+import router from '@/router';
 import adapter from 'webrtc-adapter';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+import {
+  library
+} from '@fortawesome/fontawesome-svg-core';
 
-Vue.config.productionTip = false
+import {
+  FontAwesomeIcon
+} from '@fortawesome/vue-fontawesome';
+
+import {
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUser);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
