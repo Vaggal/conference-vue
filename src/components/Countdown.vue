5e89919cf703dfdef7de8e0ecdc65398da99280c
Vangelis Galanis <vaggalanis@gmail.com>
2020-12-09 23:35:29 +0200

    feat: implement circular countdown for the timer

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>{{ minutes }}:{{ seconds }}</div>
+  <div class="base-timer">
+    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <g class="base-timer-circle">
+        <circle class="base-timer-path-elapsed" cx="50" cy="50" r="45"></circle>
+        <text x="50%" y="50%" text-anchor="middle" dy=".3em" class="svg-text">
+          {{ minutes }}:{{ seconds }}
+        </text>
+        <path
+          id="base-timer-path-remaining"
+          stroke-dasharray="283"
+          class="base-timer-path-remaining"
+          :class="currentColor"
+          d="M 50, 50
+              m -45, 0
+              a 45,45 0 1,0 90,0
+              a 45,45 0 1,0 -90,0"
+        ></path>
+      </g>
+    </svg>
+  </div>
 </template>
 
 <script>
@@ -22,6 +41,21 @@ export default {
       timer: new Timer(),
       minutes: 0,
       seconds: 0,
+      colorCodes: {
+        info: {
+          color: "green",
+        },
+        warning: {
+          color: "orange",
+          threshold: 10,
+        },
+        alert: {
+          color: "red",
+          threshold: 5,
+        },
+      },
+      currentColor: "green",
+      fullDashArray: 283,
     };
   },
   watch: {
@@ -34,6 +68,7 @@ export default {
   },
   methods: {
     startTimer() {
+      this.currentColor = this.colorCodes.info.color;
       // We always need to stop the timer as it will not start again on the next update.
       this.timer.stop();
       this.timer.start({
@@ -45,14 +80,78 @@ export default {
         countdown: true,
       });
       this.timer.addEventListener("secondsUpdated", () => {
-        // TODO: single digits 6 or 06?
         let timeValues = this.timer.getTimeValues();
         this.minutes = timeValues.minutes;
-        this.seconds = timeValues.seconds;
+        this.seconds = ("0" + timeValues.seconds).slice(-2);
+
+        let totalTimeValues = this.timer.getTotalTimeValues();
+        let timeLeft =
+          this.secondsLeft - (this.secondsLeft - totalTimeValues.seconds);
+
+        this.setCircleDasharray(timeLeft);
+        this.setRemainingPathColor(timeLeft);
       });
+    },
+    setRemainingPathColor(timeLeft) {
+      if (timeLeft <= this.colorCodes.alert.threshold) {
+        this.currentColor = this.colorCodes.alert.color;
+      } else if (timeLeft <= this.colorCodes.warning.threshold) {
+        this.currentColor = this.colorCodes.warning.color;
+      }
+    },
+
+    calculateTimeFraction(timeLeft) {
+      const rawTimeFraction = timeLeft / this.secondsLeft;
+      return rawTimeFraction - (1 / this.secondsLeft) * (1 - rawTimeFraction);
+    },
+
+    setCircleDasharray(timeLeft) {
+      const circleDasharray = `${(
+        this.calculateTimeFraction(timeLeft) * this.fullDashArray
+      ).toFixed(0)} 283`;
+      document
+        .getElementById("base-timer-path-remaining")
+        .setAttribute("stroke-dasharray", circleDasharray);
     },
   },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.base-timer-circle {
+  fill: none;
+  stroke: none;
+}
+
+.base-timer-path-elapsed {
+  stroke-width: 5px;
+  stroke: grey;
+}
+
+.base-timer-path-remaining {
+  stroke-width: 5px;
+  stroke-linecap: round;
+  transform: rotate(90deg);
+  transform-origin: center;
+  transition: 1s linear all;
+  fill-rule: nonzero;
+  stroke: currentColor;
+}
+
+.base-timer-path-remaining.green {
+  color: rgb(65, 184, 131);
+}
+
+.base-timer-path-remaining.orange {
+  color: orange;
+}
+
+.base-timer-path-remaining.red {
+  color: red;
+}
+
+.svg-text {
+  stroke: grey;
+  fill: grey;
+}
+</style>
